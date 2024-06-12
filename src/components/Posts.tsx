46ab5b91a92ceabaebae749cf4dedb64079a12fe
piyushimraw <piyush.srivastavaimraw@gmail.com>
2024-06-12 11:12:25 +0530

    fixed minor styles

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -13,9 +13,15 @@ function Posts(props: Props) {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {topPosts.map((post) => {
           return (
-            <div className="bg-gray-100 rounded-lg p-4" key={post.id}>
+            <div
+              className="bg-gray-100 rounded-lg p-4 cursor-pointer"
+              key={post.id}
+              onClick={() => window.open(`${post.url}`, "_blank")}
+            >
               <h3 className="text-lg font-bold mb-2">{post.title}</h3>
-              <p className="text-gray-600 mb-2">{post.selftext}</p>
+              <p className="text-gray-600 mb-2 text-ellipsis overflow-hidden h-4">
+                {post.selftext}
+              </p>
               <div className="flex items-center text-gray-500 text-sm">
                 <UserIcon className="w-4 h-4 mr-2" />
                 <span>u/{post.author}</span>
